a728766f457db0e0723eab66d773c6dad450ca47
Ramil <ram4ik@hotmail.com>
2022-07-16 16:51:12 +0300

    init

--- SimpleNavigationViewInMacOS/ContentView.swift
+++ SimpleNavigationViewInMacOS/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var colors: [Color] = [.red, .green, .blue, .yellow, .purple]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(1...5, id: \.self) { val in
+                    NavigationLink(destination: PageView(pageText: String(val), color: colors[val - 1])) {
+                        Text("Screen \(val)")
+                            .font(.largeTitle)
+                            .foregroundColor(colors[val - 1])
+                            .padding()
+                    }
+                }
+            }
+        }
+            
+    }
+}
+
+struct PageView: View {
+    
+    var pageText = "Test"
+    var color: Color = .black
+    
+    var body: some View {
+        VStack {
+            Text("Page: \(pageText)")
+                .font(.title)
+                .foregroundColor(color)
+        }
     }
 }
 
