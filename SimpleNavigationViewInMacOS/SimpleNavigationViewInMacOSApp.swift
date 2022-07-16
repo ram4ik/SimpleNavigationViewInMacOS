b5aabea25d4c12200768a28e263e6d9c5cfaf9b6
Ramil <ram4ik@hotmail.com>
2022-07-16 16:37:27 +0300

    Initial Commit

---
+++ SimpleNavigationViewInMacOS/SimpleNavigationViewInMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavigationViewInMacOSApp.swift
+//  SimpleNavigationViewInMacOS
+//
+//  Created by Ramill Ibragimov on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavigationViewInMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
